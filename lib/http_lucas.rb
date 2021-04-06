38ef94e1703c2646848da3d063d54bde385181f1
Lucas da Silva <lucas.silva00@outlook.ie>
2021-04-06 23:41:09 +0100

    gem finalized

--- lib/http_lucas.rb
+++ lib/http_lucas.rb
@@ -4,12 +4,15 @@ module HttpLucas
   class Error < StandardError; 
   end
 
-  @url = url
-  @params = params
-  @headers = headers
+  def self.test(param)
+    alert_message = "The gem is working. This is the Param received: " + param
+    return alert_message
+  end
 
-  def get:
-    response = Faraday.get(@url , @params, @headers)
+  def self.get(url, params, headers)
+
+    response = Faraday.get(url, params, headers)
+
     return response
   end
 
